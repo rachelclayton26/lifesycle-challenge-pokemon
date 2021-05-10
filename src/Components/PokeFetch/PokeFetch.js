3f8a2da486bc769e7f33c5b93bb7d45165cb64ab
Rachel Christopher <rachelchristopher@Rachels-MacBook-Pro.local>
2021-05-10 10:17:48 -0500

    Mon 10:17

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -7,81 +7,57 @@ class PokeFetch extends React.Component {
     this.state = {
       pokeInfo: '',
       pokeSprite: '',
-      pokeName: '',}
-    this.state = { time: {}, seconds: 10 };
-    this.timer = 0;
-    this.startTimer = this.startTimer.bind(this);
-    this.countDown = this.countDown.bind(this);
-  }
+      pokeName: '',
+      timer: {}, 
+      seconds: 6,
+      isRunning: false
+    };
+  };
 
   fetchPokemon() {
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
+
+    this.timer = setInterval(() => this.Decrement(), 1000);
+    console.log(this.timer)
+
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
       method: 'GET'
     }).then(res => res.json())
       .then(res => {
         this.setState({
+          isRunning: true,
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
+          seconds: 6
         })
       })
       .catch((err) => console.log(err));
-    this.startTimer();
+    console.log(this.state.seconds)
     }
 
-  secondsToTime(secs){
-    let hours = Math.floor(secs / (60 * 60));
-
-    let divisor_for_minutes = secs % (60 * 60);
-    let minutes = Math.floor(divisor_for_minutes / 60);
-
-    let divisor_for_seconds = divisor_for_minutes % 60;
-    let seconds = Math.ceil(divisor_for_seconds);
-
-    let obj = {
-      "h": hours,
-      "m": minutes,
-      "s": seconds
+    Decrement() {
+      if (this.state.isRunning=true && this.state.seconds > 0) {
+      this.setState((prevState) => ({
+        seconds: prevState.seconds -1 }));
+      }
+      if (this.state.seconds === 0) { 
+         this.setState({
+           isRunning: false
+          })
+      }
     };
-    return obj;
-  }
-
-  componentDidMount() {
-    let timeLeftVar = this.secondsToTime(this.state.seconds);
-    this.setState({ time: timeLeftVar });
-  }
-
-  startTimer() {
-    if (this.timer == 0 && this.state.seconds > 0) {
-      this.timer = setInterval(this.countDown, 1000);
-    }
-  }
-
-  countDown() {
-    // Remove one second, set state so a re-render happens.
-    let seconds = this.state.seconds - 1;
-    this.setState({
-      time: this.secondsToTime(seconds),
-      seconds: seconds,
-    });
-    
-    // Check if we're at zero.
-    if (seconds == 0) { 
-      clearInterval(this.timer);
-    }
-  }
 
   render() {
         return (
           <div className={'wrapper'}>
             <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-            <h1 className={'timer'} >Timer Display: {this.state.time.s}</h1>
+            <h1 className={this.state.isRunning ? "timer" : 'timerOff'} >Timer Display: {this.state.seconds}</h1>
             <div className={'pokeWrap'}>
-              <img className={this.state.timer = 0 ? 'pokeImg' : "pokeImgDark"} src={this.state.pokeSprite} alt="" />
-              <h1 className={'pokeName'}>{this.state.timer = 0 ? this.state.pokeName : ''}</h1>
+              <img className={this.state.isRunning ? "pokeImgDark" : 'pokeImg'} src={this.state.pokeSprite} alt="" />
+              <h1 className={'pokeName'}>{this.state.isRunning ? '' : this.state.pokeName}</h1>
             </div>
           </div>
         )
